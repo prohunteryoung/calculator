84006d0dd6f972d6c6170c317089089348a77644
Yeongnam <productivery@naver.com>
2022-12-21 13:28:49 +0900

    Dirty code

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-function add(numbs) {
-  let sum = 0;
-  for (let i = 0; i < numbs.length; i++) {
-    sum += numbs[i];
-  }
-}
-function sub(a, b) {
-  return a - b;
-}
-function multiply(a, b) {
-  return a * b;
-}
-function divide(a, b) {
-  return a / b;
-}
-
-function operate(op, num1, num2) {
-  if (op === "+") displayValue.textContent = add(numbs);
-  else if (op === "-") return sub(num1, num2);
-  else if (op === "*") return multiply(num1, num2);
-  else if (op === "/") return divide(num1, num2);
+function operate(op, a, b) {
+  if (op === "+") return a + b;
+  else if (op === "-") return a - b;
+  else if (op === "*") return a * b;
+  else if (op === "/") return a / b;
 }
 
 // Display Number
@@ -45,6 +29,13 @@ function displayNumber(n) {
   displayValue.textContent = displayedNumber;
   displayedNumber = Number(displayedNumber);
 }
+function editNumber() {
+  displayedNumber = displayedNumber.toString();
+  displayedNumber = displayedNumber.slice(0, displayedNumber.length - 1);
+  if (displayedNumber.length === 0) displayedNumber = 0;
+  displayValue.textContent = displayedNumber;
+}
+
 no1.addEventListener("click", () => displayNumber("1"));
 no2.addEventListener("click", () => displayNumber("2"));
 no3.addEventListener("click", () => displayNumber("3"));
@@ -55,19 +46,21 @@ no7.addEventListener("click", () => displayNumber("7"));
 no8.addEventListener("click", () => displayNumber("8"));
 no9.addEventListener("click", () => displayNumber("9"));
 no0.addEventListener("click", () => displayNumber("0"));
-edit.addEventListener("click", () => {
-  displayedNumber = displayedNumber.toString();
-  displayedNumber = displayedNumber.slice(0, displayedNumber.length - 1);
-  if (displayedNumber.length === 0) displayedNumber = 0;
-  displayValue.textContent = displayedNumber;
-});
+edit.addEventListener("click", editNumber);
+
+let inputs = [];
+let operators = [];
 
 // Clear Button
-const clearBtn = document.querySelector(".clear");
-clearBtn.addEventListener("click", () => {
+function clearFunction() {
   displayedNumber = 0;
   displayValue.textContent = displayedNumber;
-});
+  inputs = [];
+  operators = [];
+}
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearFunction);
 
 // Operate Buttons
 const plus = document.querySelector(".add");
@@ -76,54 +69,66 @@ const mult = document.querySelector(".multiply");
 const div = document.querySelector(".divide");
 const equal = document.querySelector(".equal");
 
-const inputs = [];
-const operators = [];
-
 function storeInput() {
   inputs.push(displayedNumber);
   displayedNumber = 0;
 }
+
 function inputOperator(operator) {
   operators.push(operator);
 }
 
-const calcStatus = document.querySelector(".status");
-let yourStatus = "";
-
-function statusUpdate() {
-  for (let i = 0; i < inputs.length; i++) {
-    yourStatus += inputs[i];
-    yourStatus += operators[i];
-  }
-  calcStatus.textContent = yourStatus;
+function plusFunction() {
+  inputOperator("+");
+  storeInput();
+  statusUpdate();
 }
 
 plus.addEventListener("click", () => {
-  storeInput();
-  inputOperator("+");
-  statusUpdate();
+  plusFunction();
 });
 minus.addEventListener("click", () => {
-  storeInput();
   inputOperator("-");
+  storeInput();
   statusUpdate();
 });
 mult.addEventListener("click", () => {
-  storeInput();
   inputOperator("*");
+  storeInput();
   statusUpdate();
 });
 div.addEventListener("click", () => {
-  storeInput();
   inputOperator("/");
+  storeInput();
   statusUpdate();
 });
 
+const calc = [];
+function calculate() {
+  storeInput();
+  yourStatus = calc.join(" ");
+  calcStatus.textContent = yourStatus;
+}
+
 equal.addEventListener("click", () => {
-  inputs.push(displayedNumber);
-  console.log(inputs);
-  console.log(operators[0], inputs[0], inputs[1]);
-  console.log(operate(operators[0], inputs[0], inputs[1]));
+  calculate();
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "1") return displayNumber("1");
+  if (e.key === "2") return displayNumber("2");
+  if (e.key === "3") return displayNumber("3");
+  if (e.key === "4") return displayNumber("4");
+  if (e.key === "5") return displayNumber("5");
+  if (e.key === "6") return displayNumber("6");
+  if (e.key === "7") return displayNumber("7");
+  if (e.key === "8") return displayNumber("8");
+  if (e.key === "9") return displayNumber("9");
+  if (e.key === "0") return displayNumber("0");
+  if (e.key === "Escape") return clearFunction();
+  if (e.key === "Backspace") return editNumber();
+
+  if (e.key === "+") return plusFunction();
 });
 
 // 입력한 값을 배열에 저장하는 것 까지 완료.
